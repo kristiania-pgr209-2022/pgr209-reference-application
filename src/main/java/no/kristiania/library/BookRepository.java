505a4427c32f43039184f3023fc0b307f765dbba
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-07-24 21:45:08 +0200

    POST /api/books action (bug fixed)

--- src/main/java/no/kristiania/library/BookRepository.java
+++ src/main/java/no/kristiania/library/BookRepository.java
@@ -1,11 +1,12 @@
 package no.kristiania.library;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class BookRepository {
 
-    private final List<String> books = List.of("Book1", "Book2");
+    private final List<String> books = new ArrayList<>(List.of("Book1", "Book2"));
 
     public Stream<String> streamAllBooks() {
         return books.stream();
