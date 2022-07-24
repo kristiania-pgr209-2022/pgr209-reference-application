11743a8f8bf4c7d3e8261620314ea5c0711dbc0c
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-07-24 15:51:38 +0200

    Setup logging

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,8 +1,12 @@
 package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LibraryServer {
+    private static final Logger logger = LoggerFactory.getLogger(LibraryServer.class);
+
     private final Server server = new Server(0);
 
     public static void main(String[] args) throws Exception {
@@ -11,6 +15,6 @@ public class LibraryServer {
 
     private void start() throws Exception {
         server.start();
-        System.out.println("Started LibraryServer at " + server.getURI());
+        logger.info("Started {} at {}", this.getClass().getSimpleName(), server.getURI());
     }
 }
