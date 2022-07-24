2df95aef0345a7edb182aa86c2619b11ea34b4b3
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-07-24 23:45:45 +0200

    🟢 AuthorRepositoryTest.shouldListSavedAuthors

--- src/main/java/no/kristiania/library/authors/AuthorRepository.java
+++ src/main/java/no/kristiania/library/authors/AuthorRepository.java
@@ -1,9 +1,11 @@
 package no.kristiania.library.authors;
 
+import org.fluentjdbc.DatabaseRow;
 import org.fluentjdbc.DatabaseSaveResult;
 import org.fluentjdbc.DbContext;
 import org.fluentjdbc.DbContextTable;
 
+import java.sql.SQLException;
 import java.util.stream.Stream;
 
 public class AuthorRepository {
@@ -22,14 +24,17 @@ public class AuthorRepository {
     }
 
     public Stream<Author> streamAll() {
-        return table.query().stream(row -> {
-            Author author = new Author();
-            author.setFullName(row.getString("full_name"));
-            return author;
-        });
+        return table.query().stream(this::mapToAuthor);
     }
 
-    public Author retrieve(Long authorId) {
-        return null;
+    private Author mapToAuthor(DatabaseRow row) throws SQLException {
+        Author author = new Author();
+        author.setId(row.getLong("id"));
+        author.setFullName(row.getString("full_name"));
+        return author;
+    }
+
+    public Author retrieve(Long id) {
+        return table.query().where("id", id).singleObject(this::mapToAuthor).orElseThrow();
     }
 }
