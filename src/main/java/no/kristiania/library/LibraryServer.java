a7cb987a5370fab8fbeb74cc6578909742366df5
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-07-24 15:44:28 +0200

    Micro refactor

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -3,6 +3,10 @@ package no.kristiania.library;
 public class LibraryServer {
 
     public static void main(String[] args) {
+        new LibraryServer().start();
+    }
+
+    private void start() {
         System.out.println("Hello LibraryServer");
     }
 }
