96afae2239c39508ceec3e9b5c82cc4bfa8a6cf4
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-07-24 23:27:21 +0200

    🟢 Add AuthorRepositoryTest.shouldListSavedAuthors

--- src/main/resources/db/migration/V002__create_table_authors.sql
+++ src/main/resources/db/migration/V002__create_table_authors.sql
@@ -1,8 +1,7 @@
-create table books
+create table authors
 (
     id         serial primary key,
     created_at date         not null default now(),
     updated_at date         not null default now(),
-    title      varchar(200) not null,
-    author     varchar(200)
+    full_name  varchar(200) not null
 )
