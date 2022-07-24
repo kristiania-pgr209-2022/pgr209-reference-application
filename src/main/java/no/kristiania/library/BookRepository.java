8164c070a1b5a1c298b6bbc10dc8a199f564adf2
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-07-24 22:20:05 +0200

    Refactor BookRepository to parallel store books and bookTitles

--- src/main/java/no/kristiania/library/BookRepository.java
+++ src/main/java/no/kristiania/library/BookRepository.java
@@ -1,18 +1,25 @@
 package no.kristiania.library;
 
+import org.eclipse.jetty.http.HttpFields;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class BookRepository {
 
-    private final List<String> books = new ArrayList<>(List.of("Book1", "Book2"));
+    private final List<String> bookTitles = new ArrayList<>(List.of("Book1", "Book2"));
+    private final List<Book> books = new ArrayList<>();
 
     public Stream<String> streamAllBooks() {
-        return books.stream();
+        return books.stream().map(Book::getTitle);
     }
 
     public void insertBook(String title, String author) {
-        books.add(title);
+        Book book = new Book();
+        book.setTitle(title);
+        book.setAuthor(author);
+        books.add(book);
+        bookTitles.add(title);
     }
 }
